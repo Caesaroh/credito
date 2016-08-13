0b0313e1a38eda0ac30fcc4d4333c4441d2a20fd
César Higuera <cesarhigeura9111@gmail.com>
2016-08-12 22:22:17 -0600

    Templates, scripts y estilos

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-/**
- * Created by César on 12/08/2016.
- */
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/static'));
+
+app.listen(3000, function() {
+    console.log("App is listening at port 3000!");
+});
