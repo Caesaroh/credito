028d2121fd59d6f67f7aff3997067ad32ff596e7
César Higuera <cesarhigeura9111@gmail.com>
2016-08-16 21:57:13 -0600

    Modelos Producto y Cliente

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var clientes = require('./routes/clientes');
+
 var app = express();
 
-app.use('/', express.static(__dirname + '/static'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.listen(3000, function() {
-    console.log("App is listening at port 3000!");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/clientes', clientes);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
